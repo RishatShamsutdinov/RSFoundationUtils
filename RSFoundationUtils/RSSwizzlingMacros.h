81e17f8c3d78432fd95f881772342f0da8cd6896
Rishat Shamsutdinov <dichat.dark@gmail.com>
2016-09-29 12:38:08 +0500

    add ability to swizzle method using selector

--- RSFoundationUtils/RSSwizzlingMacros.h
+++ RSFoundationUtils/RSSwizzlingMacros.h
@@ -27,10 +27,12 @@
 #define RS_SWIZZLED_METHOD(name, ...) RS_SWIZZLED_IMP(name)(id self, SEL _cmd, __VA_ARGS__)
 #define RS_SWIZZLED_METHOD_WO_ARGS(name) RS_SWIZZLED_IMP(name)(id self, SEL _cmd)
 
-#define RS_SWIZZLE(clazz, selectorName, name) \
-    Method name ## Method = class_getInstanceMethod((clazz), @selector((selectorName))); \
-    \
-    RS_ORIGINAL_IMP(name) = method_setImplementation(name ## Method, (IMP)RS_SWIZZLED_IMP(name))
+#define RS_SWIZZLE_SEL(clazz, sel, name) \
+    Method name ## Method = class_getInstanceMethod((clazz), (sel));                                  \
+                                                                                                    \
+    RS_ORIGINAL_IMP(name) = method_setImplementation(name ## Method, (IMP)RS_SWIZZLED_IMP(name))    \
+
+#define RS_SWIZZLE(clazz, selectorName, name) RS_SWIZZLE_SEL((clazz), @selector((selectorName)), (name))
 
 #define RS_INVOKE_ORIGINAL_IMP0(returnType, name) \
     ((returnType (*)(id, SEL))RS_ORIGINAL_IMP(name))(self, _cmd)
